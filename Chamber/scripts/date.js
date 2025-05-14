d684688cf2f37b14e93ba0833f1ffd2d005c9a53
STU6266 <skemendics@byupathway.edu>
2025-05-14 21:16:50 +0200

    deleted:    Chamber/data/members.json 	deleted:    Chamber/directory.html 	deleted:    Chamber/images/facebook.png 	deleted:    Chamber/images/favicon.ico 	deleted:    Chamber/scripts/date.js 	deleted:    Chamber/scripts/members.js 	deleted:    Chamber/scripts/navigation.js 	deleted:    Chamber/styles/large.css 	deleted:    images/austria.jpg 	deleted:    images/dom.webp 	deleted:    images/profil.png 	deleted:    images/weather-icon.svg 	deleted:    images/wien.jpg 	chamber/

--- Chamber/scripts/date.js
+++
@@ -1,6 +0,0 @@
-
-const yearEl     = document.getElementById('currentYear');
-const modifiedEl = document.getElementById('lastModified');
-
-yearEl.textContent     = new Date().getFullYear();
-modifiedEl.textContent = `Last Update: ${document.lastModified}`;
